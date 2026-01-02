95a85e120919154d93d9951673cac0eae6c4f965
youcode safi <kaoutar@gmail.com>
2026-01-02 21:22:45 +0100

    quelque mod

--- app/views/Admin/AfficherReader.views.php
+++ app/views/Admin/AfficherReader.views.php
@@ -38,7 +38,7 @@ if(isset($_SESSION['reader'])){
     <div class="overflow-x-auto">
       <table class="min-w-full">
         <thead>
-          <tr class="bg-gray-800 text-white">
+          <tr class="bg-blue-800 text-white">
             <th class="px-6 py-4 text-left text-sm font-semibold uppercase tracking-wider rounded-tl-lg">
               Nom
             </th>
